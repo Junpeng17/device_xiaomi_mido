6168c38e977075a2726412dde9e9900eb2a805a1
Junpeng17 <32558644+Junpeng17@users.noreply.github.com>
2019-10-13 10:42:07 +0800

    mido: Add Authsecret HAL
    不知道有什么用，随便加的
    看log有这个：LockSettingsService: Device doesn't implement AuthSecret HAL

--- device.mk
+++ device.mk
@@ -168,6 +168,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_mido
 
+PRODUCT_PACKAGES += \
+    android.hardware.authsecret@1.0-service
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
